b9a2d0ae388dcbed7f24d89d2eea4acb9609fbb1
createname02 <danchoi113.tn@gmail.com>
2024-01-22 15:04:50 +0700

    done aihiii

--- app/src/main/java/com/example/testcheck/MainActivity.kt
+++ app/src/main/java/com/example/testcheck/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.testcheck
 
 import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.content.res.AssetManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.util.Log
+import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -18,9 +25,70 @@ import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var imageView1: ImageView
+    private lateinit var imageView2: ImageView
+    private lateinit var lineView: View
+    private var currentImageIndex = 1
 
+    private val totalWidth: Float by lazy {
+        imageView1.width.toFloat() + imageView2.width.toFloat()
+    }
+
+    private val duration = 5000L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imageView1 = findViewById(R.id.imageView1)
+        imageView2 = findViewById(R.id.imageView2)
+        lineView = findViewById(R.id.lineView)
+
+
+        imageView2.visibility = View.INVISIBLE
+
+        val fadeIn = ObjectAnimator.ofFloat(imageView1, "alpha", 1f, 0f)
+        fadeIn.duration = 1000L // Thời gian di chuyển của ảnh (ms)
+
+        val fadeOut = ObjectAnimator.ofFloat(imageView2, "alpha", 0f, 1f)
+        fadeOut.duration = 1000L
+
+        val lineMoveAnimator = ValueAnimator.ofFloat(0f, totalWidth)
+        lineMoveAnimator.duration = 1000L
+        lineMoveAnimator.addUpdateListener { valueAnimator ->
+            val translationX = valueAnimator.animatedValue as Float
+            lineView.translationX = translationX
+
+            val alpha = 1f - (translationX / totalWidth)
+            imageView2.alpha = alpha
+
+            // Kiểm tra điều kiện để ẩn hiện ảnh khi thanh line đi qua một vị trí cụ thể
+            val conditionPosition = totalWidth / 2
+            if (translationX >= conditionPosition) {
+                imageView1.visibility = View.INVISIBLE
+                imageView2.visibility = View.VISIBLE
+            } else {
+                imageView1.visibility = View.VISIBLE
+                imageView2.visibility = View.INVISIBLE
+            }
+        }
+
+        val animatorSet = AnimatorSet()
+        animatorSet.playSequentially(fadeIn, fadeOut)
+        animatorSet.start()
+
+        lineMoveAnimator.start()
+    }
+    private fun imageSwap() {
+        val temp = imageView1
+        imageView1 = imageView2
+        imageView2 = temp
+
+        imageView1.alpha = 1f
+        imageView2.alpha = 0f
+
+        // Đặt lại vị trí và alpha của thanh line khi chuyển ảnh
+        lineView.translationX = 0f
+        lineView.alpha = 1f
     }
 }
+
+
--- app/src/main/java/com/example/testcheck/MainActivity2.kt
+++ app/src/main/java/com/example/testcheck/MainActivity2.kt
@@ -1,27 +1,43 @@
 package com.example.testcheck
+
+import android.animation.ObjectAnimator
+import android.content.Context
+import android.content.res.AssetManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.AsyncTask
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
 import android.view.animation.AccelerateDecelerateInterpolator
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.airbnb.lottie.LottieAnimationView
+import com.airbnb.lottie.LottieComposition
+import com.airbnb.lottie.LottieDrawable
+import kotlinx.coroutines.Job
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+import kotlin.math.log
+
 
 class MainActivity2 : AppCompatActivity() {
     private val imagePairs: MutableList<Pair<Bitmap, Bitmap>> = mutableListOf()
     private var isAnimationInProgress = false
     private val handler = Handler()
+    private var job: Job? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-
         setupImagePairs()
         upDataImageJson(imagePairs[2])
-    }
 
+    }
+//    thêm ảnh vào bitmap
     private fun setupImagePairs() {
         addImagePair(R.drawable.after, R.drawable.before)
         addImagePair(R.drawable.after1, R.drawable.before1)
@@ -35,14 +51,16 @@ class MainActivity2 : AppCompatActivity() {
     }
 
     private fun upDataImageJson(imagePair: Pair<Bitmap, Bitmap>) {
+
         if (isAnimationInProgress) {
             return // Avoid starting a new animation while the previous one is still in progress
         }
         isAnimationInProgress = true
 
+//        val desiredWidth = resources.displayMetrics.run { widthPixels / density }
+        val desiredWidth = 360
         val lavChest = findViewById<LottieAnimationView>(R.id.aimation)
-        val density = resources.displayMetrics.density
-        val desiredWidth = resources.displayMetrics.run { widthPixels / density }
+
         val desiredHeight = 221 // Replace with your desired height
 
         val resizedBitmapAfter: Bitmap = Bitmap.createScaledBitmap(
@@ -61,10 +79,9 @@ class MainActivity2 : AppCompatActivity() {
             isAnimationInProgress = true
             lavChest.speed = 1f
             lavChest.playAnimation()
-        }, -200)
+        }, 100)
     }
 
-    @Suppress("DEPRECATION")
     inner class LoadImagesAsyncTask(
         private val lavChest: LottieAnimationView,
         private val resizedBitmapAfter: Bitmap,
@@ -72,7 +89,6 @@ class MainActivity2 : AppCompatActivity() {
         private val imagePair: Pair<Bitmap, Bitmap>
     ) : AsyncTask<Void, Void, Void>() {
 
-        @Deprecated("Deprecated in Java")
         override fun doInBackground(vararg params: Void?): Void? {
             // Perform heavy work here (e.g., applying animation)
             try {
@@ -84,11 +100,11 @@ class MainActivity2 : AppCompatActivity() {
             return null
         }
 
-        @Deprecated("Deprecated in Java")
         override fun onProgressUpdate(vararg values: Void?) {
             // Update UI with the results (e.g., update LottieAnimationView)
             lavChest.updateBitmap("image_0", resizedBitmapAfter)
             lavChest.updateBitmap("image_1", resizedBitmapBefore)
+
         }
         override fun onPostExecute(result: Void?) {
             // AsyncTask finished, you can initiate the next task or perform other actions

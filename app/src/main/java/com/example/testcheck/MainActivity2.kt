f3ab10673f9543e3bf96c159ffc2930cefb8fe15
createname02 <danchoi113.tn@gmail.com>
2024-01-23 00:09:40 +0700

    done aihiii

--- app/src/main/java/com/example/testcheck/MainActivity2.kt
+++ app/src/main/java/com/example/testcheck/MainActivity2.kt
@@ -15,7 +15,12 @@ import androidx.appcompat.app.AppCompatActivity
 import com.airbnb.lottie.LottieAnimationView
 import com.airbnb.lottie.LottieComposition
 import com.airbnb.lottie.LottieDrawable
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.json.JSONException
 import org.json.JSONObject
 import java.io.BufferedReader
@@ -39,9 +44,9 @@ class MainActivity2 : AppCompatActivity() {
     }
 //    thêm ảnh vào bitmap
     private fun setupImagePairs() {
-        addImagePair(R.drawable.after, R.drawable.before)
-        addImagePair(R.drawable.after1, R.drawable.before1)
-        addImagePair(R.drawable.after2, R.drawable.before2)
+        addImagePair(R.drawable.b, R.drawable.a)
+        addImagePair(R.drawable.b1, R.drawable.a1)
+        addImagePair(R.drawable.b2, R.drawable.a2)
     }
 
     private fun addImagePair(afterResourceId: Int, beforeResourceId: Int) {
@@ -58,10 +63,10 @@ class MainActivity2 : AppCompatActivity() {
         isAnimationInProgress = true
 
 //        val desiredWidth = resources.displayMetrics.run { widthPixels / density }
-        val desiredWidth = 360
+        val desiredWidth = 1000
         val lavChest = findViewById<LottieAnimationView>(R.id.aimation)
 
-        val desiredHeight = 221 // Replace with your desired height
+        val desiredHeight = 614 // Replace with your desired height
 
         val resizedBitmapAfter: Bitmap = Bitmap.createScaledBitmap(
             imagePair.first,
@@ -71,47 +76,20 @@ class MainActivity2 : AppCompatActivity() {
             imagePair.second,
             desiredWidth.toInt(), desiredHeight, true
         )
-
-
-        // Execute AsyncTask to perform heavy work on a background thread
-        handler.postDelayed({
-            LoadImagesAsyncTask(lavChest, resizedBitmapAfter, resizedBitmapBefore, imagePair).execute()
-            isAnimationInProgress = true
-            lavChest.speed = 1f
-            lavChest.playAnimation()
-        }, 100)
-    }
-
-    inner class LoadImagesAsyncTask(
-        private val lavChest: LottieAnimationView,
-        private val resizedBitmapAfter: Bitmap,
-        private val resizedBitmapBefore: Bitmap,
-        private val imagePair: Pair<Bitmap, Bitmap>
-    ) : AsyncTask<Void, Void, Void>() {
-
-        override fun doInBackground(vararg params: Void?): Void? {
-            // Perform heavy work here (e.g., applying animation)
-            try {
-                Thread.sleep(2500)
-            } catch (e: InterruptedException) {
-                e.printStackTrace()
+        job = CoroutineScope(Dispatchers.Default).launch {
+            delay(2500) // Simulate heavy work
+            withContext(Dispatchers.Main) {
+                // Update UI with the results (e.g., update LottieAnimationView)
+                lavChest.updateBitmap("image_0", resizedBitmapAfter)
+                lavChest.updateBitmap("image_1", resizedBitmapBefore)
+                lavChest.speed = 1f
+                lavChest.playAnimation()
+                isAnimationInProgress = false
+                // Start the next animation after the current one ends
+                val nextImagePairIndex = (imagePairs.indexOf(imagePair) + 1) % imagePairs.size
+                upDataImageJson(imagePairs[nextImagePairIndex])
             }
-            publishProgress()
-            return null
-        }
-
-        override fun onProgressUpdate(vararg values: Void?) {
-            // Update UI with the results (e.g., update LottieAnimationView)
-            lavChest.updateBitmap("image_0", resizedBitmapAfter)
-            lavChest.updateBitmap("image_1", resizedBitmapBefore)
-
-        }
-        override fun onPostExecute(result: Void?) {
-            // AsyncTask finished, you can initiate the next task or perform other actions
-            isAnimationInProgress = false
-            // Start the next animation after the current one ends
-            val nextImagePairIndex = (imagePairs.indexOf(imagePair) + 1) % imagePairs.size
-            upDataImageJson(imagePairs[nextImagePairIndex])
         }
     }
+
 }
